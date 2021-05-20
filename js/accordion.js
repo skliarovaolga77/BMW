529430014fb49e29002fc90efbed3b1eee2aaf01
Olga Skliarova <skliarovaolga77@gmail.com>
2021-05-20 11:00:03 +0300

    accordion.js:dz_1

---
+++ js/accordion.js
@@ -0,0 +1,18 @@
+const featureLinkElems = document.querySelectorAll('.feature__link');
+const featureSubElems = document.querySelectorAll('.feature-sub');
+
+featureLinkElems.forEach((btn, index) => {
+  btn.addEventListener('click' ,() =>{
+    featureSubElems.forEach((elem, i)=>{
+      //если i==index открыли-закрыли ul на к-й кликнули иначе закрыли ul 
+      i === index ? elem.classList.toggle('hidden') : elem.classList.add('hidden');
+    });
+    //повернули стрелку при клике 
+     //если i==index повернули стрелку при клике вниз иначе повернули стрелку  вверх
+    featureLinkElems.forEach((elem, i)=>{
+      i === index ? elem.classList.toggle('feature__link_active') : elem.classList.remove('feature__link_active');
+    });
+  });
+   
+  
+});
